f6a8482a63ddf1339811494645f6f37406577cd4
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-16 19:37:14 +0100

    Aggiunge tabella pivot restaurant_kitchen

--- database/migrations/2023_02_16_140900_create_restaurants_table.php
+++ database/migrations/2023_02_16_140900_create_restaurants_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->unique();
-            $table->string('address', 150);
+            $table->string('address');
             $table->string('vat_number', 11);
             $table->string('image')->nullable();
-            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,6 +31,9 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::table('restaurants', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
         Schema::dropIfExists('restaurants');
     }
 };
