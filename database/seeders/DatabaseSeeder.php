07a25e0b4ad9f8cace7270d499a17fa8a1bc6881
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-17 09:49:16 +0100

    Elimina seeder table pivot KitchenRestaurant

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,8 +26,6 @@ class DatabaseSeeder extends Seeder
             OrderSeeder::class,
             KitchenSeeder::class,
             OrderProductSeeder::class,
-            KitchenRestaurantSeeder::class,
         ]);
-
     }
 }
