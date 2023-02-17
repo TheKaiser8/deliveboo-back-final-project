ae58a55b32467b1175f5338c7f2c2bf78c4bf949
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-17 12:16:29 +0100

    Aggiunge dati seeder in Kitchen

--- database/seeders/KitchenSeeder.php
+++ database/seeders/KitchenSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kitchen;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class KitchenSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class KitchenSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Metodo truncate() per ripopolare da zero il seeder (Kitchen in questo caso) ogni volta che viene rilanciato
+        Schema::disableForeignKeyConstraints();
+        Kitchen::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $kitchens = ['italiano', 'internazionale', 'cinese', 'giapponese', 'messicano', 'indiano', 'coreano', 'francese', 'thailandese', 'australiano'];
+
+        for ($i = 0; $i < count($kitchens); $i++) {
+            $new_kitchen = new Kitchen();
+            $new_kitchen->name = $kitchens[$i];
+            $new_kitchen->save();
+        }
     }
 }
