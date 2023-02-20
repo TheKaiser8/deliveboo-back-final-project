cbe0373ca862fc56933fba779538e9ca62360967
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-02-20 14:29:18 +0100

    Add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Schema;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        for($i = 0; $i < 5 ; $i++){
+            $new_user = new User();
+            $new_user->name = $faker->name();
+            $new_user->email = $faker->email();
+            $new_user->password = 'password';
+            $new_user->save();
+        }
+    }
+}
