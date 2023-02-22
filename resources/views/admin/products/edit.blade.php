b02f864a5870be5e137d974626e60c8d3c917615
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-22 17:37:06 +0100

    Add gestione errori in edit product

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -7,67 +7,74 @@
 @section('content')
     <div class="container mt-5">
         <h1>Modifica prodotto</h1>
-        {{-- @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div> 
-        @endif  --}}
-        <form action="{{route('admin.products.update', $product)}}" method="POST" enctype="multipart/form-data" class="mb-5"> {{-- se non usassi l'enctype mi verrebbe restituito solo il nome dell'immagine --}}
+        {{-- form di modifica prodotto --}}
+        <form action="{{route('admin.products.update', $product)}}" method="POST" enctype="multipart/form-data" class="mb-5">
         @csrf
         @method('PUT')
-        @error($errors->any())
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-
-
+            {{-- campo nome --}}
             <div class="mb-3">
-               <label for="name" class="form-label"><h6>Nome*</h6></label>
-               <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci il nome di questo piatto" value="{{ old('name', $product->name) }}">
+                <label for="name" class="form-label"><h6>Nome*</h6></label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Inserisci il nome di questo piatto" value="{{old('name', $product->name)}}">
+                @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-            <div class="mb-3 w-25">
+             {{-- campo disponibilità --}}
+             <div class="mb-3 w-25">
                 <label for="is_available" class="form-label"><h6>Disponibilità*</h6></label>
-                <select name="is_available" id="is_available" class="form-select">
+                <select name="is_available" id="is_available" class="form-select @error('is_available') is-invalid @enderror">
                     <option value="1" {{ old('is_available', $product->is_available) == 1 ? 'selected' : ''}}>Disponibile</option>
                     <option value="0" {{ old('is_available', $product->is_available) == 0 ? 'selected' : ''}}>Non Disponibile</option>
                 </select>
+                @error('is_available')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+            {{-- campo tipologia --}}
             <div class="mb-3 w-25">
                 <label for="typology" class="form-label"><h6>Tipologia*</h6></label>
-                <select name="typology" id="typology" class="form-select">
-                    <option value="cibo" {{ old('typology', $product->typology) == 'cibo' ? 'selected' : ''}}>Cibo</option>
-                    <option value="bevanda" {{ old('typology', $product->typology) == 'bevanda' ? 'selected' : ''}}>Bevanda</option>
+                <select name="typology" id="typology" class="form-select @error('typology') is-invalid @enderror">
+                    <option value="" selected>Seleziona una tipologia</option>
+                    <option value="panini" {{ old('typology', $product->typology) == 'panini' ? 'selected' : ''}}>Panini</option>
+                    <option value="pizza" {{ old('typology', $product->typology) == 'pizza' ? 'selected' : ''}}>Pizza</option>
+                    <option value="sushi" {{ old('typology', $product->typology) == 'sushi' ? 'selected' : ''}}>Sushi</option>
+                    <option value="vegetariano" {{ old('typology', $product->typology) == 'vegetariano' ? 'selected' : ''}}>Vegetariano</option>
+                    <option value="dessert" {{ old('typology', $product->typology) == 'dessert' ? 'selected' : ''}}>Dessert</option>
+                    <option value="bevande" {{ old('typology', $product->typology) == 'bevande' ? 'selected' : ''}}>Bevande</option>
+                    <option value="alcolici" {{ old('typology', $product->typology) == 'alcolici' ? 'selected' : ''}}>Alcolici</option>
                 </select>
+                @error('typology')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-            <div class="mb-3 w-25">
-                <label for="dish" class="form-label"><h6>Portata*</h6></label>
-                <select name="dish" id="dish" class="form-select">
-                    <option value="primo" {{ old('dish', $product->dish) === 'primo' ? 'selected' : '' }}>Primo</option>
-                    <option value="secondo" {{ old('dish', $product->dish) === 'secondo' ? 'selected' : ''}}>Secondo</option>
-                    <option value="contorno" {{ old('dish', $product->dish) === 'contorno' ? 'selected' : ''}}>Contorno</option>
-                    <option value="dessert" {{ old('dish', $product->dish) === 'dessert' ? 'selected' : ''}}>Dessert</option>
-                </select>
-            </div>
+            {{-- campo descrizione --}}
             <div class="mb-3">
                 <label for="description" class="form-label"><h6>Descrizione Piatto*</h6></label>
-                <textarea class="form-control" id="description" name="description" rows="5" placeholder="Descrivi il tuo piatto...">{{ old('description', $product->description) }}</textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="5" placeholder="Descrivi il tuo piatto...">{{old('description', $product->description)}}</textarea>
+                @error('description')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+            {{-- campo ingredienti --}}
             <div class="mb-3">
-                <label for="ingredients" class="form-label"><h6>Elenco Ingredienti*</h6></label>
-                <textarea class="form-control" id="ingredients" name="ingredients" rows="5" placeholder="Elenca gli ingedienti del tuo piatto...">{{ old('ingredients', $product->ingredients) }}</textarea>
+                <label for="ingredients" class="form-label @error('ingredients') is-invalid @enderror"><h6>Elenco Ingredienti*</h6></label>
+                <textarea class="form-control" id="ingredients" name="ingredients" rows="5" placeholder="Elenca gli ingedienti del tuo piatto...">{{old('ingredients', $product->ingredients)}}</textarea>
+                @error('ingredients')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-
-             <div class="mb-3">
-                <label for="price" class="form-label"><h6>Prezzo*</h6></label>
-                <input type="number" class="form-control" id="price" name="price" min="0" value="{{ old('price', $product->price) }}" step=".01" max="9999.99" placeholder="00,00">
+            {{-- campo prezzo --}}
+            <div class="mb-3">
+                <label for="price" class="form-label @error('price') is-invalid @enderror"><h6>Prezzo*</h6></label>
+                <input type="number" class="form-control" id="price" name="price" value="{{ old('price', $product->price) }}" min="0" step=".01" max="99.99" placeholder="00,00">
+                @error('price')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-
+            {{-- campo immagine --}}
             <div class="mb-3 w-25">
                 <label for="image" class="form-label"><h6>Immagine Piatto</h6></label> 
-                
+                {{-- preview immagine  --}}
                 <div>
                     <img id="output" width="100" class="mb-2"/>
                     <script>
@@ -81,16 +88,17 @@
                         };
                     </script>
                 </div>
-                
-                <input type="file" class="form-control" id="image" name="image" value="{{old('image', $product->image)}}" onchange="loadFile(event)">
+                {{-- /preview immagine  --}}
+                <input type="file" class="form-control @error('image') is-invalid @enderror" id="image" name="image" value="{{old('image', $product->image)}}" onchange="loadFile(event)">
+                @error('image')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-            
-            <button type="submit" class="btn btn-success">Conferma</button>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-success me-2">Conferma</button>
+                <a href="{{ route('admin.products.index')}}" class="btn btn-primary">Ritorna ai prodotti</a>
+            </div>
         </form>
-
-        <div>
-            <a href="{{ route('admin.products.index')}}" class="btn btn-primary">Return to Projects List</a>
-        </div>
-        
+        {{-- /form di modifica prodotto --}}
     </div>
 @endsection
