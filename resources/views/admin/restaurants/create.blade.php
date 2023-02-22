3471d0f23affeb54ae818b3de69064cc017ef77f
Therakar <matteo.pau200@gmail.com>
2023-02-22 19:10:51 +0100

    Aggiunge gestione errori in create restaurant

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -6,41 +6,51 @@
 
 @section('content')
     <h2>Crea un Nuovo Ristorante</h2>
-
     <div class="container mt-5">
         <h1>Crea un Nuovo Ristorante</h1>
-        {{-- @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div> 
-        @endif  --}}
         <form action="{{route('admin.restaurants.store')}}" method="POST" enctype="multipart/form-data" class="mb-5"> {{-- se non usassi l'enctype mi verrebbe restituito solo il nome dell'immagine --}}
         @csrf
+            {{-- campo nome --}}
             <div class="mb-3">
                <label for="name" class="form-label"><h6>Nome*</h6></label>
-               <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci il nome del tuo ristorante" value="{{old('name')}}">
+               <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Inserisci il nome del tuo ristorante" value="{{old('name')}}">
+               @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+            {{-- campo città --}}
             <div class="mb-3">
                 <label for="city" class="form-label"><h6>Città*</h6></label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Inserisci la città del tuo ristorante" value="{{old('city')}}">
+                <input type="text" class="form-control @error('city') is-invalid @enderror" id="city" name="city" placeholder="Inserisci la città del tuo ristorante" value="{{old('city')}}">
+                @error('city')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+            {{-- campo indirizzo --}}
             <div class="mb-3">
                 <label for="street_address" class="form-label"><h6>Indirizzo*</h6></label>
-                <input type="text" class="form-control" id="street_address" name="street_address" placeholder="Inserisci l'indirizzo del tuo ristorante" value="{{old('street_address')}}">
+                <input type="text" class="form-control @error('street_address') is-invalid @enderror" id="street_address" name="street_address" placeholder="Inserisci l'indirizzo del tuo ristorante" value="{{old('street_address')}}">
+                @error('street_address')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+            {{-- campo codice postale --}}
             <div class="mb-3">
                 <label for="postal_code" class="form-label"><h6>Codice Postale*</h6></label>
-                <input type="text" class="form-control" id="postal_code" name="postal_code" placeholder="Inserisci il codice postale" value="{{old('postal_code')}}">
+                <input type="text" class="form-control @error('postal_code') is-invalid @enderror" id="postal_code" name="postal_code" placeholder="Inserisci il codice postale" value="{{old('postal_code')}}">
+                @error('postal_code')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+            {{-- campo partita iva --}}
             <div class="mb-3">
                 <label for="vat_number" class="form-label"><h6>Partita IVA*</h6></label>
-                <input type="text" class="form-control" id="vat_number" name="vat_number" placeholder="Inserisci la partita IVA" value="{{old('vat_number')}}">
+                <input type="text" class="form-control @error('vat_number') is-invalid @enderror" id="vat_number" name="vat_number" placeholder="Inserisci la partita IVA" value="{{old('vat_number')}}">
+                @error('vat_number')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-            
+            {{-- campo immagine --}}
             <div class="mb-3 w-25">
                 <label for="image" class="form-label"><h6>Copertina Ristorante</h6></label> 
 
@@ -58,25 +68,26 @@
                         };
                     </script>
                 </div>
-                
+                {{-- /preview immagine  --}}
                 <input type="file" class="form-control" id="image" name="image" value="{{old('image')}}" onchange="loadFile(event)">
             </div>
-            
+            {{-- campo cucine --}}
             <div class="mb-3">
-                <h6>Cucine</h6>
+                <h6>Cucine*</h6>
                 @foreach ($kitchens as $kitchen)
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" id="{{$kitchen->id}}" name="kitchens[]" value="{{$kitchen->id}}" {{ in_array($kitchen->id, old('kitchens', []) ) ? 'checked' : ''}}>
-                        <label class="form-check-label" for="{{$kitchen->id}}">{{$kitchen->name}}</label>
+                        <label class="form-check-label @error('kitchens') is-invalid @enderror" for="{{$kitchen->id}}">{{$kitchen->name}}</label>
                     </div>
                 @endforeach
+                @error('kitchens')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
-            <button type="submit" class="btn btn-success">Conferma</button>
+            <div>
+                <a href="{{ route('admin.restaurants.index')}}" class="btn btn-primary">Ritorna al  tuo ristorante</a>
+                <button type="submit" class="btn btn-success">Conferma</button>
+            </div>
         </form>
-
-        {{-- <div>
-            <a href="{{ route('admin.projects.index')}}" class="btn btn-primary">Return to Projects List</a>
-        </div> --}}
-        
     </div>
 @endsection
