abb9b79faccfdfa1fa762f2520ce98544d7768a2
Therakar <matteo.pau200@gmail.com>
2023-02-24 10:51:01 +0100

    add product index page with table products

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -5,6 +5,44 @@
 @endsection
 
 @section('content')
-    <h2>I tuoi prodotti</h2>
-    <a href="{{ route('admin.products.create')}}" class="btn btn-primary">Crea prodotto</a>
+    <div class="container mt-3">
+        <h2 class="mb-3">I tuoi prodotti</h2>
+        <a href="{{ route('admin.products.create')}}" class="btn btn-primary mb-3">Crea prodotto</a>
+
+        @if(count($products) < 1)
+            <h2 class="mb-3">Non hai ancora creato alcun prodotto!</h2> 
+        @else
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Tipologia</th>
+                        <th scope="col">Disponibilità</th>
+                        <th scope="col">Prezzo</th>
+                        <th scope="col">Azioni</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->typology}}</td>
+                            <td>{{ $product->is_available == 1 ? 'Disponibile' : 'Non disponibile'}}</td>
+                            <td>{{ $product->price}}</td>
+                            <td>
+                                <a href="{{ route('admin.products.show', $product) }}" class="btn btn-light"><i class="fa-solid fa-eye"></i></a>
+                                <a href="{{ route('admin.products.edit', $product) }}" class="btn btn-light"><i class="fa-solid fa-pen-to-square"></i></a>
+            
+                                <!-- Button modale (modalDelete) -->
+                                <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#modalDelete-{{ $product->id }}"><i class="fa-solid fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
+    
 @endsection
