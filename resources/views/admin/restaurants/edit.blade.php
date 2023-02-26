d823b301644ae8c329b02ef937ceaf3adbe3a7f7
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-27 00:38:43 +0100

    Refactoring style restaurant edit page

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.admin')
 
 @section('page-title')
-    Modifica 
+    Modifica ristorante
 @endsection
 
 @section('content')
-    <div class="container mt-5">
-        <h1>Modifica il tuo Ristorante</h1>
+    <div class="container py-4">
+        <h2 class="fw-semibold text-center mb-4">Modifica "{{ $restaurant->name }}"</h2>
+        {{-- form di modifica ristorante --}}
         <form action="{{ route('admin.restaurants.update', $restaurant) }}" method="POST" enctype="multipart/form-data" class="mb-5">
         @csrf
         @method('PUT')
-            {{-- campo name --}}
+            {{-- campo nome --}}
             <div class="mb-3">
-               <label for="name" class="form-label"><h6>Nome*</h6></label>
+               <label for="name" class="form-label fw-semibold mb-2">Nome*</label>
                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Inserisci il nome del tuo ristorante" value="{{ old('name', $restaurant->name) }}" maxlength="100" required>
                @error('name')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
-            {{-- campo city --}}
-            <div class="mb-3">
-                <label for="city" class="form-label"><h6>Città*</h6></label>
+            {{-- campo città --}}
+            <div class="mb-3 w-50">
+                <label for="city" class="form-label fw-semibold mb-2">Città*</label>
                 <input type="text" class="form-control @error('city') is-invalid @enderror" id="city" name="city" placeholder="Inserisci la città del tuo ristorante" value="{{old('city', $restaurant->city)}}" maxlength="50" required>
                 @error('city')
                     <div class="alert alert-danger">{{ $message }}</div>
@@ -28,34 +29,34 @@
             </div>
             {{-- campo indirizzo --}}
             <div class="mb-3">
-                <label for="street_address" class="form-label"><h6>Indirizzo*</h6></label>
+                <label for="street_address" class="form-label fw-semibold mb-2">Indirizzo*</label>
                 <input type="text" class="form-control @error('street_address') is-invalid @enderror" id="street_address" name="street_address" placeholder="Inserisci l'indirizzo del tuo ristorante" value="{{old('street_address', $restaurant->street_address)}}" maxlength="100" required>
                 @error('street_address')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             {{-- campo codice postale --}}
-            <div class="mb-3">
-                <label for="postal_code" class="form-label"><h6>Codice Postale*</h6></label>
+            <div class="mb-3 w-25">
+                <label for="postal_code" class="form-label fw-semibold mb-2">Codice Postale*</label>
                 <input type="text" class="form-control @error('postal_code') is-invalid @enderror" id="postal_code" name="postal_code" placeholder="Inserisci il codice postale" value="{{old('postal_code', $restaurant->postal_code)}}" minlength="5" maxlength="5" required>
                 @error('postal_code')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             {{-- campo partita iva --}}
-            <div class="mb-3">
-                <label for="vat_number" class="form-label"><h6>Partita IVA*</h6></label>
+            <div class="mb-3 w-25">
+                <label for="vat_number" class="form-label fw-semibold mb-2">Partita IVA*</label>
                 <input type="text" class="form-control @error('vat_number') is-invalid @enderror" id="vat_number" name="vat_number" placeholder="Inserisci la partita IVA" value="{{ old('vat_number', $restaurant->vat_number) }}" minlength="11" maxlength="11" required>
                 @error('vat_number')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             {{-- campo immagine --}}
-            <div class="mb-3">
-                <label for="image" class="form-label"><h6>Copertina Ristorante</h6></label>
+            <div class="mb-3 w-50">
+                <label for="image" class="form-label fw-semibold mb-0">Copertina ristorante</label>
                 {{-- image preview --}}
-                <div>
-                    <img id="output" width="150" class="mb-2"/>
+                <div class="ms-lh-0">
+                    <img id="output" width="150" class="my-2"/>
                     <script>
                         let loadFile = function(event) {
                             let reader = new FileReader();
@@ -75,7 +76,7 @@
             </div>
             {{-- campo cucine --}}
             <div class="mb-3">
-                <h6>Cucina/e*</h6>
+                <h6 class="fw-semibold mb-2">Cucina/e*</h6>
                 @foreach ($kitchens as $kitchen)
                     <div class="form-check form-check-inline">
                         @if( $errors->any() )
@@ -92,9 +93,9 @@
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-success">Conferma</button>
-                <a href="{{ route('admin.restaurants.show', Auth::user()->restaurant)}}" class="btn btn-primary">Ritorna al tuo ristorante</a>
+            <div class="mt-4">
+                <button type="submit" class="btn btn-success fw-semibold me-1"><i class="fa-solid fa-check me-1"></i> Conferma</button>
+                <a href="{{ route('admin.restaurants.show', Auth::user()->restaurant)}}" class="btn btn-primary fw-semibold"><i class="fa-solid fa-delete-left me-1"></i> Annulla</a>
             </div>
         </form>
     </div>
