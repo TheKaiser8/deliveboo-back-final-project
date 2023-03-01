a0bb26b0e6d888740cca45992c206f46d860e0cf
Therakar <matteo.pau200@gmail.com>
2023-03-01 20:41:47 +0100

    fix OrderController phone_number

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -17,7 +17,7 @@ class OrderController extends Controller
         $new_order-> name_customer= $data['name_customer'];
         $new_order-> total_price= $data['total_price'];
         $new_order-> email_customer= $data['email_customer'];
-        $new_order-> phone_number= $data['phone_customer'];
+        $new_order-> phone_number= $data['phone_number'];
         $new_order-> address_customer= $data['address_customer'];
         $new_order->save();
 
