a71514097576ca8c6dc45ef7c2d652abba456889
Therakar <matteo.pau200@gmail.com>
2023-03-02 18:19:20 +0100

    delete searchbar dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,52 +34,47 @@
       <header
         class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow"
       >
-      @if( Auth::user()->restaurant )
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('admin.restaurants.show', Auth::user()->restaurant) }}"
-          >DeliveBoo</a
-        >
-      @else
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('admin.restaurants.create') }}"
-          >DeliveBoo</a
-        >
-      @endif
-        <button
-          class="navbar-toggler position-absolute d-md-none collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#sidebarMenu"
-          aria-controls="sidebarMenu"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <input
-          class="form-control form-control-dark w-100"
-          type="text"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <div class="navbar-nav">
-          <div class="nav-item text-nowrap ms-2">
-            <a
-              class="nav-link"
-              href="{{ route('logout') }}"
-              onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();"
-            >
-              {{ __('Logout') }}
-            </a>
-            <form
-              id="logout-form"
-              action="{{ route('logout') }}"
-              method="POST"
-              class="d-none"
-            >
-              @csrf
-            </form>
+        @if( Auth::user()->restaurant )
+          <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('admin.restaurants.show', Auth::user()->restaurant) }}"
+            >DeliveBoo</a
+          >
+        @else
+          <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('admin.restaurants.create') }}"
+            >DeliveBoo</a
+          >
+        @endif
+          <button
+            class="navbar-toggler position-absolute d-md-none collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu"
+            aria-controls="sidebarMenu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="navbar-nav">
+            <div class="nav-item text-nowrap ms-2">
+              <a
+                class="nav-link"
+                href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();"
+              >
+                {{ __('Logout') }}
+              </a>
+              <form
+                id="logout-form"
+                action="{{ route('logout') }}"
+                method="POST"
+                class="d-none"
+              >
+                @csrf
+              </form>
+            </div>
           </div>
-        </div>
       </header>
 
       <div class="container-fluid vh-100">
