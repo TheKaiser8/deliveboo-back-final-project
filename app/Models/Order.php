dcdb974f91f4c58611632c46171fa3f95956ed6b
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-03-03 14:48:00 +0100

    Fix refusi in order model e order index page

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,8 +10,9 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
-    public function products(){
-        return $this->belongsToMany('App\Models\Product', 'order_product', 'order_id', 'product_id')->withPivot('quantity');
 
+    public function products()
+    {
+        return $this->belongsToMany('App\Models\Product', 'order_product', 'order_id', 'product_id')->withPivot('quantity');
     }
 }
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -30,40 +30,13 @@
                     @foreach ($orders as $order)
                         <tr class="align-middle">
                             <td>{{ $order->name_customer }}</td>
-                            <td>{{ $order->total_price . '€'}}</td>
+                            <td>{{ $order->total_price . ' €'}}</td>
                             <td>{{ $order->email_customer }}</td>
                             <td>{{ $order->address_customer }}</td>
                             <td>{{ $order->phone_number }}</td>              
                             <td>{{ $order->created_at }}</td>
                             <td class="text-center"><a href="{{ route('admin.orders.show', $order) }}" class="btn btn-outline-info ms-white-hover my-1"><i class="fa-solid fa-eye"></i></a></td>
-                            {{-- <td>
-                                
-                                <!-- Button modale (modalDelete) -->
-                                <button type="button" class="btn btn-outline-danger ms-white-hover my-1" data-bs-toggle="modal" data-bs-target="#modalDelete-{{ $order->id }}"><i class="fa-solid fa-trash"></i></button>
-                            </td> --}}
                         </tr>
-                        
-                        <!-- Modale (modalDelete) -->
-                        {{-- <div class="modal fade" id="modalDelete-{{ $order->id }}" tabindex="-1" aria-labelledby="modalDeleteLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="modalDeleteLabel">Cancellazione Prodotto</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">Confermi di voler cancellare definitivamente l'ordine <strong>"{{ $order->customer_name }}"</strong>?</div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary fw-semibold" data-bs-dismiss="modal">Annulla</button>
-                                        <form action="{{ route('admin.orders.destroy', $order)}}" method="POST" class="d-inline-block">
-                                            @csrf
-                                            @method('DELETE')
-
-                                            <button type="submit" class="btn btn-danger fw-semibold">Sì, cancella!</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> --}}
                     @endforeach
                 </tbody>
             </table>
