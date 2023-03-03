66bec2132c8b463df4bf442dea30cdbdfb611dbd
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-03-03 14:46:43 +0100

    Aggiunge order show page

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -5,8 +5,55 @@
 @endsection
 
 @section('content')
-    <div class="container py-4">
-        
+    <div class="container d-flex flex-column align-items-center py-4">
+        <h2 class="fw-semibold text-center mb-4">Dettagli ordine</h2>
+        <div class="card w-75 rounded-top">
+            <div class="card-body">
+                <div class="bg-light rounded-top p-3">
+                    <h4 class="card-title text-center fw-bold mb-3">ID ordine #{{ $order->id}}</h4>
+                    <div class="mb-3">
+                        <strong>Totale ordine:</strong> 
+                        <strong class="text-success">{{ $order->total_price . ' €' }}</strong>
+                    </div>
+                    <div class="mb-3">
+                        <strong>Nome cliente:</strong> {{ $order->name_customer }}
+                    </div>
+                    <div class="mb-3">
+                        <strong>Email cliente:</strong> {{ $order->email_customer }}
+                    </div>
+                    <div class="mb-3">
+                        <strong>Recapito telefonico cliente:</strong>
+                        {{ $order->phone_number }}
+                    </div>
+                    <div class="mb-3">
+                        <strong>Indirizzo cliente:</strong>
+                        {{ $order->address_customer }}
+                    </div>
+                    <table class="ms-table table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Prodotti ordinati</th>
+                                <th scope="col" class="text-center">Prezzo unitario</th>
+                                <th scope="col" class="text-center">Quantità</th>
+                                <th scope="col" class="text-center">Totale prodotto</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($order->products as $product)
+                                <tr class="align-middle">
+                                    <td>{{ $product->name }}</td>
+                                    <td class="text-center">{{ $product->price . ' €' }}</td>
+                                    <td class="text-center">{{ $product->pivot->quantity }}</td>
+                                    <td class="text-center">{{ $product->price * ($product->pivot->quantity) . ' €' }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="text-center bg-light rounded-bottom pb-3">
+                    <a href="{{ route('admin.orders.index') }}" class="btn btn-primary fw-semibold m-1"><i class="fa-solid fa-arrow-left me-1"></i> Torna agli ordini</a>
+                </div>
+            </div>
+        </div>
     </div>
-    
 @endsection
