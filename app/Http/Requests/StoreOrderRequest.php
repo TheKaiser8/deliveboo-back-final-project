1c3efc6b1aed2490fc3ab4a7e3879acc9d579233
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-03-04 03:36:52 +0100

    Crea validazione per dati ordine

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -13,18 +13,22 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, mixed>
      */
     public function rules()
     {
         return [
-            //
+            'name_customer' => 'required|max:100',
+            'address_customer' => 'required|max:150',
+            'email_customer' => 'required|email|max:100',
+            'phone_number' => 'required|max:30',
+            'total_price' => 'required|numeric|between:0.00,999.99',
+            'delivery_date' => 'required|date|after:now'
         ];
     }
 }
