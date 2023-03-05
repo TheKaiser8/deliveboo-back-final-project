ac2f4bd8f92c9e6d3095c4b46848cfbbb30a69b2
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-03-05 14:27:44 +0100

    Fix placeholder url on restaurant model&controller

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -11,12 +11,15 @@ class Restaurant extends Model
 
     protected $guarded = ['user_id'];
     protected $appends = ['image_url'];
+    public static $restaurantPlaceholderUrl = "https://www.ilborghista.it/immaginiutente/attivita_foto/300_m_32915-ath0q9p5q6b3m7b3b8p7x9k3x4v9q4k5b5w3l1z1d6k5q1g6p3k7.jpg?a=9192";
 
     // metodo per aggiungere una proprietà al model se non abbiamo una colonna a database con l'obiettivo di restituire l'url completo dell'immagine
     protected function getImageUrlAttribute()
     {
         if (str_starts_with($this->image, "uploads")) {
-            return $this->image ? asset("storage/$this->image") : "https://www.ilborghista.it/immaginiutente/attivita_foto/300_m_32915-ath0q9p5q6b3m7b3b8p7x9k3x4v9q4k5b5w3l1z1d6k5q1g6p3k7.jpg?a=9192";
+            return asset("storage/$this->image");
+        } else {
+            return self::$restaurantPlaceholderUrl;
         }
     }
 
