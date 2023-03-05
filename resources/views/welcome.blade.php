4b949584353253d00860c390674d16aa308994b0
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-03-06 00:46:15 +0100

    Refactoring welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,19 +5,17 @@
 @endsection
 
 @section('content')
-
-<div class="jumbotron p-5 pb-0 my-6 rounded-3">
-    <div class="container py-5">
-        <div class="welcome-logo m-auto">
-            <img src="{{Vite::asset('resources/assets/images/Deliveroo-Logo-final.png')}}" alt="logo">
+    <div class="jumbotron p-5 pb-0 my-6 rounded-3">
+        <div class="container py-5">
+            <div class="welcome-logo pb-5">
+                <img class="img-fluid" src="{{Vite::asset('resources/assets/images/Deliveroo-Logo-final.png')}}" alt="logo">
+            </div>
+            <div class="col-md-5">
+                <h1 class=" welcome-title display-5 fw-bold">
+                    Benvenuto su DeliveBoo
+                </h1>
+                <p class="welcome-text col-md-8 fs-4">Una volta registrato potrai creare il tuo ristorante e gestire menù e ordini come vorrai!</p>
+            </div>
         </div>
-        <h1 class=" welcome-title display-5 fw-bold">
-            Benvenuto su DeliveBoo 
-        </h1>
-
-        <p class="welcome-text col-md-8 fs-4">Una volta registrato potrai creare il tuo ristorante e gestire menù e ordini come vorrai!</p>
-
-        
     </div>
-</div>
 @endsection
